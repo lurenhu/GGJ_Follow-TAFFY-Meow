442c94d0daf766c61efdfb58e0533d79e36e05a0
GD_Hui_HUAWEI_MATE <109047008+lurenhu@users.noreply.github.com>
2024-01-27 17:52:25 +0800

    bug 小修复

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -14,6 +14,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         AudioCtrl.GetInstance.UISoundFunc(Botton);
+        Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
 
--- Assets/Scripts/UI/TimeCounter.cs
+++ Assets/Scripts/UI/TimeCounter.cs
@@ -32,6 +32,8 @@ public class TimeCounter : MonoBehaviour
             totalTime--;
             if (totalTime == 0) 
             {
+                PauseMenu.gameIsPause = true;
+                Time.timeScale = 0;
                 Lose.SetActive(true);
             }
 
