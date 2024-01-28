d37fd09232ff82c521434b0255ebb3b4b2484207
GD_Hui_HUAWEI_MATE <109047008+lurenhu@users.noreply.github.com>
2024-01-28 16:15:58 +0800

    角色脸部胜利失败结算变化

--- Assets/Scripts/Player/CheckHealth.cs
+++ Assets/Scripts/Player/CheckHealth.cs
@@ -7,77 +7,120 @@ using UnityEngine;
 
 public class CheckHealth : MonoBehaviour
 {
-    public bool isPlayer;
-    public HealthSystem myHealth;
-    public SpriteRenderer playerHead;
-    public SpriteRenderer enemyHead;
-
+    public Transform Player;
+    public Transform Enemy;
     public GameObject UILose;
     public GameObject UIWin;
-
     public Sprite[] Images;
+
+    public float playerMaxHealth;
+    public float enemyMaxHealth;
+
+    SpriteRenderer playerSprite;
+    SpriteRenderer enemySprite;
+    HealthSystem playerHealth;
+    HealthSystem enemyHealth;
+    bool END = false;
+
     [Range(0,100)] public static int odds;
 
+    private void Start() {
+        playerSprite = Player.Find("PlayerBody/Head/HeadSpriteAnchor/HeadSprite").GetComponent<SpriteRenderer>();
+        enemySprite = Enemy.Find("EnemyBody/Head/HeadSpriteAnchor/HeadSprite").GetComponent<SpriteRenderer>();
+
+        playerHealth = Player.GetComponentInChildren<HealthSystem>();
+        enemyHealth = Enemy.GetComponentInChildren<HealthSystem>();
+    }
+
     private void Update() {
-        if (isPlayer)
+        if (!END)
         {
             CheckPlayer();
+            CheckAI();
         }else
         {
-            CheckAI();
+            CheckWiner();
         }
+    }
+
+    private void CheckWiner()
+    {
+        if (playerHealth.getCurrentHealth == 0)
+        {
+            Lose();
+        }
+        else if(enemyHealth.getCurrentHealth == 0)
+        {
+            Win();
+        }
+        else
+        {
+            Debug.Log("No Winner");
+        }
+
     }
 
     private void CheckPlayer()
     {
-        if (myHealth.getCurrentHealth > 145)
+        if (playerHealth.getCurrentHealth > playerMaxHealth * 0.5f)
         {
-            playerHead.sprite = Images[0];
+            playerSprite.sprite = Images[0];
         }
-        else if(myHealth.getCurrentHealth <= 145 && myHealth.getCurrentHealth > 0)
+        else if(playerHealth.getCurrentHealth <= playerMaxHealth * 0.5 && playerHealth.getCurrentHealth > 0)
         {
-            playerHead.sprite = Images[1];
+            playerSprite.sprite = Images[1];
         }
         else
         {
-            playerHead.sprite = Images[2];
-            enemyHead.sprite = Images[3];
-
-            PauseMenu.gameIsPause = true;
-            Time.timeScale = 0;
-            UILose.SetActive(true);
+            END = true;
         }
+    }
+
+    private void Lose()
+    {
+        playerSprite.sprite = Images[2];
+        enemySprite.sprite = Images[3];
+
+        PauseMenu.gameIsPause = true;
+        Time.timeScale = 0;
+        UILose.SetActive(true);
     }
 
     private void CheckAI()
     {
-        if (myHealth.getCurrentHealth > 145)
+        if (enemyHealth.getCurrentHealth > 145)
         {
-            if (myHealth.getCurrentHealth > 203)
+            if (enemyHealth.getCurrentHealth > 203)
                 odds = 90;
             else 
             {
                 odds = 75;
             }
-            enemyHead.sprite = Images[3];
+            enemySprite.sprite = Images[3];
         }
-        else if(myHealth.getCurrentHealth <= 145 && myHealth.getCurrentHealth > 0)
+        else if(enemyHealth.getCurrentHealth <= 145 && enemyHealth.getCurrentHealth > 0)
         {
-            if (myHealth.getCurrentHealth > 87)
+            if (enemyHealth.getCurrentHealth > 87)
                 odds = 68;
             else
             {
                 odds = 60;
             }
-            enemyHead.sprite = Images[4];
+            enemySprite.sprite = Images[4];
         }
         else
         {
-            enemyHead.sprite = Images[5];
-            playerHead.sprite = Images[0];
-            PauseMenu.gameIsPause = true;
-            Time.timeScale = 0;
-            UIWin.SetActive(true);
+            END = true;
         }
+    }
+
+    private void Win()
+    {
+        playerSprite.sprite = Images[0];
+        enemySprite.sprite = Images[5];
+
+        PauseMenu.gameIsPause = true;
+        Time.timeScale = 0;
+        UIWin.SetActive(true);
     }
 }
