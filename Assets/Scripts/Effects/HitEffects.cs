9fd5fe6f86d8061ac6816835be15c20baa5031e5
Harold Lever <Harold_Lever@outlook.com>
2024-01-28 13:56:20 +0800

    新增角色受击抖动

--- Assets/Scripts/Effects/HitEffects.cs
+++ Assets/Scripts/Effects/HitEffects.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
@@ -18,6 +19,19 @@ public class HitEffects : MonoBehaviour
 
     public event Action<Vector2, float, HurtType, HitEffectsData> OnHitEvent = delegate{};
 
+    private Vector3 shakeTransformOriginalPosition;
+    private float shakeTransformAngle = 0.0f;
+    private float transformShakeIntensity = 0.0f;
+
+
+    private void Start()
+    {
+        if (hitEffectsData.shakeTransform)
+        {
+            shakeTransformOriginalPosition = hitEffectsData.shakeTransform.localPosition;
+        }
+        
+    }
 
     private void OnEnable()
     {
@@ -32,6 +46,53 @@ public class HitEffects : MonoBehaviour
     public void Play(Vector2 hitPosition, float hitSpeed, HurtType hurtType)
     {
         OnHitEvent.Invoke(hitPosition, hitSpeed, hurtType, hitEffectsData);
+        ShakeTransform(hitSpeed * hitEffectsData.transformShakeFactor);
+    }
+
+    private void ShakeTransform(float intensity)
+    {
+        if (!hitEffectsData.shakeTransform) return;
+        
+        StopCoroutine(ShakeTransformCoroutine(hitEffectsData.shakeTransform));
+        StopCoroutine(ShakeTransformFadeCoroutine(hitEffectsData.shakeTransform));
+        transformShakeIntensity = intensity;
+        StartCoroutine(ShakeTransformCoroutine(hitEffectsData.shakeTransform));
+    }
+
+    private IEnumerator ShakeTransformCoroutine(Transform transform)
+    {
+        StartCoroutine(ShakeTransformFadeCoroutine(transform));
+        float timeAccumulation = 1.0f/hitEffectsData.transformShakeFrameRate;
+        while (transformShakeIntensity > 0)
+        {
+            if (timeAccumulation >= 1.0f/hitEffectsData.transformShakeFrameRate)
+            {
+                timeAccumulation = 0.0f;
+                shakeTransformAngle += UnityEngine.Random.Range(60.0f, 300.0f);
+                shakeTransformAngle %= 360.0f;
+                Vector3 transformShakeVector = new Vector2(
+                    math.cos(shakeTransformAngle/360.0f*2*math.PI),
+                    math.sin(shakeTransformAngle/360.0f*2*math.PI)
+                ) * transformShakeIntensity;
+                transform.localPosition = shakeTransformOriginalPosition + transformShakeVector;
+            }
+            else
+            {
+                timeAccumulation += Time.deltaTime;
+            }
+            yield return null;
+        }
+        transform.localPosition = shakeTransformOriginalPosition;
+    }
+
+    private IEnumerator ShakeTransformFadeCoroutine(Transform transform)
+    {
+        transformShakeIntensity = math.max(transformShakeIntensity, 0.0f);
+        while (transformShakeIntensity > 0)
+        {
+            transformShakeIntensity -= Time.deltaTime;
+            yield return null;
+        }
     }
 
     [Serializable]
@@ -43,5 +104,10 @@ public class HitEffects : MonoBehaviour
         public float componentStartSpeedFactor = 3.0f;
         [Tooltip("零件数量系数")] 
         public float componentCountFactor = 0.5f;
+        [Tooltip("抖动物体")]
+        public Transform shakeTransform;
+        public float transformShakeFactor = 0.01f;
+
+        public float transformShakeFrameRate = 15.0f;
     }
 }
